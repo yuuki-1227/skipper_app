d7c36c128792093dd10bbb17e0f09559fe438e95
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-20 07:20:59 +0000

    [Add]jQueryクリックアクション,skipperスライドショー

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,32 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var title = "javascriptが使えました";
+alert(title);
+
+$(document).ready(function(){
+  $('.jquery').on('click',function(){
+    $(this).css('color','red');
+  });
+});
+
+$(document).ready(function(){
+  $("#theTarget").skippr({
+    transtion: 'slide',
+    speed: 1000,
+    easing: 'easeOutQuart',
+    navType: 'block',
+    childrenElementType :'div',
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuraiton: 3000,
+    keyboardOnAlways: true,
+    hidePrevious: false
+  });
+});
